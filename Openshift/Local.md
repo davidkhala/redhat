34be2a16e0bb7a9cfdffd61623af44fa25208396
David Liu <david-khala@hotmail.com>
2025-02-26 12:39:32 +0800

    Add Setup and Run

--- Openshift/Local.md
+++ Openshift/Local.md
@@ -7,7 +7,7 @@ https://console.redhat.com/openshift/create/local
 
 1. Get the binary
   - [Windows:.msi](https://cn9yc2hk0gzg.objectstorage.ap-singapore-1.oci.customer-oci.com/n/cn9yc2hk0gzg/b/install-binary/o/redhat%2Fopenshift%2Fcrc-windows-amd64.msi)
-    - 
+
 2. Install and `crc setup`
 3. **pull secret** is required in `crc start`
 
--- Openshift/local.ps1
+++ Openshift/local.ps1
@@ -1,9 +1,31 @@
+function Fetch {
+    $Uri = "https://cn9yc2hk0gzg.objectstorage.ap-singapore-1.oci.customer-oci.com/n/cn9yc2hk0gzg/b/install-binary/o/redhat%2Fopenshift%2Fcrc-windows-amd64.msi"
+    $Path = "crc-windows-amd64.msi"
+    Invoke-WebRequest -Uri $Uri -OutFile $Path -UseBasicParsing
+}
 function Setup {
-    crc setup
+    # crc should be run in a shell without administrator rights
+    Write-Output yes | crc setup
+}
+function Setup-AsRoot {
+    # Force crc to be run in a shell with administrator rights
+    crc config set skip-check-administrator-user true
+    Setup
 }
 
-function Start {
-    crc start
+function Run {
+    # File path of image pull secret (download from https://console.redhat.com/openshift/create/local)
+    param(
+        $PullSecret
+    )
+    
+    if ([string]::IsNullOrEmpty($PullSecret)) {
+        # Interactive
+        crc start
+    }
+    else {
+        crc start -p $PullSecret
+    }
 }
 if ($args.Count -gt 0) {
     Invoke-Expression ($args -join " ")
