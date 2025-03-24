ed492e746d5241dd89bbcd4940b489d0a52ee997
David Liu <david-khala@hotmail.com>
2025-03-24 19:17:54 +0800

    fix chmod

--- Openshift/local.sh
+++ Openshift/local.sh
@@ -1,12 +1,24 @@
 # for linux
 
 set -e
-download() {
+install() {
     curl -o crc https://objectstorage.ap-singapore-1.oraclecloud.com/n/cn9yc2hk0gzg/b/install-binary/o/redhat%2Fopenshift%2Fcrc
+    chmod +x crc
     sudo mv crc /usr/local/bin
+    echo y | crc setup
+    sudo reboot
+}
+
+run() {
+    local PullSecret=$1
+    if [ -z "$PullSecret" ]; then
+        crc start
+    else
+        crc start -p $PullSecret
+    fi
 }
 ubuntu-preset() {
-    sudo apt install -y virtiofsd virt-manager
+    sudo apt install -y curl virtiofsd virt-manager
 }
 
 open() {
