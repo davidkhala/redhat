245cc5ee9c9cde93958ce827c9e179853de7172e
David Liu <david-khala@hotmail.com>
2025-04-18 19:42:24 +0800

    add 4.17 install

--- Openshift/local/local.sh
+++ Openshift/local/local.sh
@@ -2,10 +2,23 @@
 
 set -e
 install() {
-    curl -o crc https://objectstorage.ap-singapore-1.oraclecloud.com/n/cn9yc2hk0gzg/b/install-binary/o/redhat%2Fopenshift%2Fcrc
-    chmod +x crc
-    sudo mv crc /usr/local/bin
+    wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.49.0/crc-linux-amd64.tar.xz
+    tar xf crc-linux-amd64.tar.xz
+    chmod +x crc-linux-2.49.0-amd64/crc
+    sudo mv crc-linux-2.49.0-amd64/crc /usr/local/bin
     echo y | crc setup
+    rm -rf crc-linux-2.49.0-amd64 crc-linux-amd64.tar.xz
+}
+
+install-stable() {
+    # Openshift 4.17
+
+    wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.47.0/crc-linux-amd64.tar.xz
+    tar xf crc-linux-amd64.tar.xz
+    chmod +x crc-linux-2.47.0-amd64/crc
+    sudo mv crc-linux-2.47.0-amd64/crc /usr/local/bin
+    echo y | crc setup
+    rm -rf crc-linux-2.47.0-amd64 crc-linux-amd64.tar.xz
 }
 
 run() {
@@ -23,6 +36,9 @@ ubuntu-install() {
     sudo reboot
     :
     crc setup
+}
+uninstall() {
+    crc cleanup
 }
 
 open() {
