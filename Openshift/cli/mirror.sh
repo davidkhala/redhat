894d34b03937f913fff35cd842944aa85e664726
David Liu <david-khala@hotmail.com>
2026-08-14 19:06:48 +0800

    Create mirror.sh

---
+++ Openshift/cli/mirror.sh
@@ -0,0 +1,13 @@
+
+download(){
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/oc-mirror.tar.gz
+  
+}
+install(){
+  tar -xvf oc-mirror.tar.gz
+  chmod +x oc-mirror
+  sudo mv oc-mirror /usr/local/bin/
+  oc-mirror version  # 验证
+
+}
+$@
